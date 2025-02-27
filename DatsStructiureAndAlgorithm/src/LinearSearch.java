8c8a824e15ece14600e8a7828d5d4f9de3e565c3
Anele Bilikwa <aceamahle1@gmail.com>
2025-02-28 00:17:32 +0200

    Sort

--- DatsStructiureAndAlgorithm/src/LinearSearch.java
+++ DatsStructiureAndAlgorithm/src/LinearSearch.java
@@ -24,7 +24,7 @@ public class LinearSearch {
     // Main method, the entry point of the program
     public static void main(String[] args) {
         // Create a String array named students with some names
-        String[] students = {"Felicia", "Vanilla", "Pedro", "Aunt", "Coco", "Cool"};
+        //[] students = {"Felicia", "Vanilla", "Pedro", "Aunt", "Coco", "Cool"};
         Integer[] numbers = {
                 1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
                 21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
