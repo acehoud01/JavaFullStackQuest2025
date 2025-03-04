1b386771bd65bcf3c1c5025700effac46332a0f0
Anele Bilikwa <aceamahle1@gmail.com>
2025-03-04 06:42:32 +0200

    Day 0

---
+++ Phase-1-Fundation/src/week_1_mini_projects/PersonalInfo.java
@@ -0,0 +1,16 @@
+package week_1_mini_projects;
+
+public class PersonalInfo {
+    public static void main(String[] args) {
+        // Variables with different data types
+        String name = "Ace";
+        int age = 0;
+        double height = 0.0;
+        char initial = 'A';
+        boolean isLeaningJava = true;
+
+        // Print them out
+        System.out.printf("Name: %s\nAge: %d\nHeight: %.1f\nInitial: %c\nLearning Java: %b", name, age,
+                height, initial, isLeaningJava);
+    }
+}
